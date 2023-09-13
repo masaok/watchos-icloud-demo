d4e942fbac6c1dc9ac7aaa96a607bbd0e436b5af
Masao Kitamura <masaok@gmail.com>
2023-09-13 01:12:19 -0700

    Initial Commit

---
+++ watchos-icloud-demo Watch App/watchos_icloud_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchos_icloud_demoApp.swift
+//  watchos-icloud-demo Watch App
+//
+//  Created by keckadmin on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct watchos_icloud_demo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
